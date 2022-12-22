d8348c376f84dae37ad66fd0523feb08a68a533e
DmitryASJS <ovir@mail.ru>
2022-12-22 16:43:01 +0300

    Добавили новую функцию Bar

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from datetime import datetime
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
 # Press the green button in the gutter to run the script.
+def Bar():
+    print(f'Текущая дата: {datetime.now()}')
+
+Bar()
+
 if __name__ == '__main__':
     print_hi('PyCharm')
 
